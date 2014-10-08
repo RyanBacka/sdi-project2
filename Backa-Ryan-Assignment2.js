f1b4bdd678720b6e250ee839d14f075cdacb5de3
RyanBacka <RKBacka@Fullsail.edu>
2014-10-08 10:34:15 -0400

    changed code in JS

--- Backa-Ryan-Assignment2.js
+++ Backa-Ryan-Assignment2.js
@@ -5,4 +5,58 @@
 //SDI 14/10
 //Project 2
 
-//Variables & functions
+//Global Varriables
+var CondiArray = new Array();
+	calculatedArray = new Array();
+	condi=true;
+	isItALetter=true;
+	totalEntries=0;
+	
+//Functions
+var getCondiNum = function(enterCondi,CondiNumArray[],entries){
+	var counter=0;
+	if (enterCondi===true){
+		while (counter!==entries){
+			CondiNumArray[counter]=prompt("How many total barrels of condensate were in hour " + (counter++) + "?");
+			If (isNan(CondiNumArray[counter])===false){
+				CondiNumArray[counter]=pareseInt(CondiNumArray[counter]);
+				console.log("You made " + CondiNumArray[counter] + " this hour.");
+				counter++;
+			}else{
+				console.log("Please enter a number!");	
+			};
+		};
+		console.log("You have entered " + counter + " numbers.");
+	}else{
+		console.log("You have no new numbers to enter.");
+	};
+	return CondiNumArray;
+};
+ 
+var CalcCondiBbls = function(entries, calcArray[], enteredCondiArray[]){
+	var loopCounter;
+	for (loopCounter = 0; loopCounter <= entries; loopCounter++){
+		calcArray[loopCounter] = enteredCondiArray[loopCounter++] - enteredCondiArray[loopCounter];
+		calcArray[loopCounter] = parseInt(calcArray[loopCounter]);
+		console.log("You made " + calcArray[loopCounter] + " barrels of condensate in hour "+(loopCounter++));
+	};
+	return calcArray;
+};
+
+//Main Code
+condi=confirm("Do you have condensate numbers to enter? click OK for yes");
+while (isItALetter===true){
+	totalEntries=prompt("How many entries do you have?");
+	if (isNan(totalEntries)=false){
+		parseInt(totalEntries);
+		isItALetter = false;
+	}else{
+		console.log("Please enter a number!");
+	};
+};
+getCondiNum(condi,CondiArray,totalEntries);
+CondiArray[]=CondiNumArray[];
+CalcCondiBbls(totalEntries,CondiArray[],calculateArray[]);
+calculatedArray[]=calcArray[];
+
+
